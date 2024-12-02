54c70fc2c0d915bfb388d9d356bcfdd1da8ac9e0
reinhardprs <reinhardprs@gmail.com>
2024-12-02 09:29:53 +0700

    Fix Login/Register Google

--- utils/google/google.go
+++ utils/google/google.go
@@ -12,7 +12,7 @@ var conf = configs.InitConfig().Google
 var GoogleOauthConfig = &oauth2.Config{
 	ClientID:     conf.ClientID,
 	ClientSecret: conf.ClientKey,
-	RedirectURL:  "http://localhost:8080/api/v1/users/google-callback",
+	RedirectURL:  "http://greenenvironment.my.id/api/v1/users/google-callback",
 	Scopes: []string{
 		"https://www.googleapis.com/auth/userinfo.profile",
 		"https://www.googleapis.com/auth/userinfo.email",
