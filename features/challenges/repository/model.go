a04c2fd5db2022a0b76e947594794163413e9075
reinhardprs <reinhardprs@gmail.com>
2024-12-15 06:57:31 +0700

    add constraint to all foreign keys

--- features/challenges/repository/model.go
+++ features/challenges/repository/model.go
@@ -9,18 +9,18 @@ import (
 
 type Challenge struct {
 	*gorm.Model
-	ID               string `gorm:"primaryKey;type:varchar(50);not null;column:id"`
-	Author           string `gorm:"type:varchar(255);not null;column:author"`
-	Title            string `gorm:"type:varchar(255);not null;column:title"`
-	Difficulty       string `gorm:"type:varchar(255);not null;column:difficulty"`
-	ChallengeImg     string `gorm:"type:varchar(255);not null;column:challenge_img"`
-	Description      string `gorm:"type:varchar(255);not null;column:description"`
-	DurationDays     int    `gorm:"type:int;not null;column:duration_days"`
-	Exp              int    `gorm:"type:int;not null;column:exp"`
-	Coin             int    `gorm:"type:int;not null;column:coin"`
-	ActionCount      int    `gorm:"type:int;not null;default:0;column:action_count"`
-	ParticipantCount int    `gorm:"type:int;not null;default:0;column:participant_count"`
-	ImpactCategories []ChallengeImpactCategory `gorm:"foreignKey:ChallengeID;references:ID"`
+	ID               string                      `gorm:"primaryKey;type:varchar(50);not null;column:id"`
+	Author           string                      `gorm:"type:varchar(255);not null;column:author"`
+	Title            string                      `gorm:"type:varchar(255);not null;column:title"`
+	Difficulty       string                      `gorm:"type:varchar(255);not null;column:difficulty"`
+	ChallengeImg     string                      `gorm:"type:varchar(255);not null;column:challenge_img"`
+	Description      string                      `gorm:"type:varchar(255);not null;column:description"`
+	DurationDays     int                         `gorm:"type:int;not null;column:duration_days"`
+	Exp              int                         `gorm:"type:int;not null;column:exp"`
+	Coin             int                         `gorm:"type:int;not null;column:coin"`
+	ActionCount      int                         `gorm:"type:int;not null;default:0;column:action_count"`
+	ParticipantCount int                         `gorm:"type:int;not null;default:0;column:participant_count"`
+	ImpactCategories []ChallengeImpactCategory   `gorm:"foreignKey:ChallengeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type ChallengeImpactCategory struct {
@@ -28,8 +28,8 @@ type ChallengeImpactCategory struct {
 	ID               string                        `gorm:"primaryKey;type:varchar(50);not null;column:id"`
 	ChallengeID      string                        `gorm:"type:varchar(50);not null;column:challenge_id"`
 	ImpactCategoryID string                        `gorm:"type:varchar(50);not null;column:impact_category_id"`
-	Challenge        Challenge                     `gorm:"foreignKey:ChallengeID;references:ID"`
-	ImpactCategory   impactcategory.ImpactCategory `gorm:"foreignKey:ImpactCategoryID;references:ID"`
+	Challenge        Challenge                     `gorm:"foreignKey:ChallengeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ImpactCategory   impactcategory.ImpactCategory `gorm:"foreignKey:ImpactCategoryID;references:ID;"`
 }
 
 type ChallengeTask struct {
@@ -39,7 +39,7 @@ type ChallengeTask struct {
 	Name            string    `gorm:"type:varchar(255);not null;column:name"`
 	DayNumber       int       `gorm:"type:int;not null;column:day_number"`
 	TaskDescription string    `gorm:"type:text;not null;column:task_description"`
-	Challenge       Challenge `gorm:"foreignKey:ChallengeID;references:ID"`
+	Challenge       Challenge `gorm:"foreignKey:ChallengeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type ChallengeLog struct {
@@ -51,7 +51,7 @@ type ChallengeLog struct {
 	StartDate    time.Time `gorm:"type:datetime;not null;column:start_date"`
 	Feed         string    `gorm:"type:text;column:feed"`
 	RewardsGiven bool      `gorm:"type:boolean;default:false;column:rewards_given"`
-	Challenge    Challenge `gorm:"foreignKey:ChallengeID;references:ID"`
+	Challenge    Challenge `gorm:"foreignKey:ChallengeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type ChallengeConfirmation struct {
@@ -62,7 +62,7 @@ type ChallengeConfirmation struct {
 	Status          string        `gorm:"type:enum('Progress', 'Done', 'Failed');not null;column:status"`
 	ChallengeImg    string        `gorm:"type:varchar(255);not null;column:challenge_img"`
 	SubmissionDate  time.Time     `gorm:"type:datetime;not null;column:submission_date"`
-	ChallengeTask   ChallengeTask `gorm:"foreignKey:ChallengeTaskID;references:ID"`
+	ChallengeTask   ChallengeTask `gorm:"foreignKey:ChallengeTaskID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (Challenge) TableName() string {
